632806fc671ce788ead8085696978d67ba1b41d2
pphanuwichh <pphanuwichh@gmail.com>
2024-04-02 06:27:31 +0700

    Created Frontend

---
+++ frontend/src/layouts/components/NavItems.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+</script>
+
+<template>
+  <VerticalNavLink
+    :item="{
+      title: 'test',
+      icon: 'ri-login-box-line',
+      to: '/cards',
+    }"
+  />
+  
+</template>
